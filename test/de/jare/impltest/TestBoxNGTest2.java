23320a911231f5e0689a3d680b4ae58672a927ca
Janusch Rentenatus <jrentenatus@outlook.de>
2026-05-03 08:58:35 +0200

    test2 review

--- test/de/jare/impltest/TestBoxNGTest2.java
+++ test/de/jare/impltest/TestBoxNGTest2.java
@@ -31,8 +31,8 @@ import static org.testng.Assert.assertNull;
 import static org.testng.Assert.fail;
 
 /**
- * Test class for the TestBox implementation.
- * Tests parsing and building of complex JSON structures with the ImplTestDefinition.
+ * Test class for the TestBox implementation. Tests parsing and building of
+ * complex JSON structures with the ImplTestDefinition.
  *
  * @author Janusch Rentenatus
  */
@@ -45,8 +45,7 @@ public class TestBoxNGTest2 {
     }
 
     /**
-     * Sets up the test class.
-     * Printed to stdout for test tracking.
+     * Sets up the test class. Printed to stdout for test tracking.
      *
      * @throws Exception If setup fails.
      */
@@ -57,8 +56,7 @@ public class TestBoxNGTest2 {
     }
 
     /**
-     * Tears down the test class.
-     * Printed to stdout for test tracking.
+     * Tears down the test class. Printed to stdout for test tracking.
      *
      * @throws Exception If teardown fails.
      */
@@ -88,8 +86,8 @@ public class TestBoxNGTest2 {
     }
 
     /**
-     * Tests parsing and building with a valid testbox configuration.
-     * Target version using the new parser pipeline.
+     * Tests parsing and building with a valid testbox configuration. Target
+     * version using the new parser pipeline.
      *
      * @param configFile The configuration file to parse.
      * @param definition The ImplTestDefinition to use.
@@ -140,7 +138,10 @@ public class TestBoxNGTest2 {
         System.out.println("subsub  = " + root.getSubsub().getText());
         System.out.println("one  = " + root.getOne().getText());
         for (ValueInterface elem : root.getArr()) {
-            System.out.println("elem  > " + elem.getText());
+            System.out.println("arr.elem  > " + elem.getText());
+        }
+        for (ValueInterface elem : root.getList()) {
+            System.out.println("list.elem  > " + elem.getText());
         }
         System.out.println("Target===============================================");
         System.out.println(root);
@@ -158,78 +159,6 @@ public class TestBoxNGTest2 {
 
         File configFile = new File("./assets/config/testbox_2.json");
         testModel(configFile, ImplTestDefinition2.getInstance());
-    }
-
-    /**
-     * Tests parsing and building with a testbox configuration containing errors.
-     * Target version using the new parser pipeline.
-     *
-     * @param configFile The configuration file to parse.
-     * @param definition The ImplTestDefinition to use.
-     * @return The built TestBox instance.
-     */
-    private Object testModel_error(File configFile, ImplTestDefinition definition) {
-        System.out.println("Target=============================================== File");
-        System.out.println(configFile.getAbsolutePath());
-
-        JsonItem obj1 = null;
-        JsonNode node = null;
-        try {
-            final JsonResource res = JsonParserService.parse(configFile, JsonDebugLevel.INFO);
-            System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWw");
-            System.out.println(res.getExpectedBox());
-            System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWw");
-            node = res.getRoot();
-            final JsonModelDescriptor descriptor = definition.getDescriptor();
-            obj1 = JsonParser.parse(res, descriptor, definition.getTestBox().getcName(), JsonDebugLevel.INFO);
-        } catch (JsonParseException | IOException | NullPointerException ex) {
-            Logger.getGlobal().log(Level.SEVERE, null, ex);
-            fail(ex.getMessage(), ex);
-        }
-        assertNotNull(obj1);
-        System.out.println("Target=============================================== Print node");
-        RootObjectWriter writer = new RootObjectWriter(definition, definition.getTestBox());
-        writer.writeNode(System.out, node);
-        System.out.println("Target=============================================== Config Class");
-        System.out.println(obj1.getClass());
-
-        TestBox root = null;
-        try {
-            final Object buildInstance1 = JsonBuilder.buildInstance(definition.getModel(), false, obj1);
-            System.out.println(buildInstance1.getClass().getName());
-            assertNotNull(root = (TestBox) buildInstance1);
-
-        } catch (JsonBuildException ex) {
-            Logger.getGlobal().log(Level.SEVERE, null, ex);
-            fail(ex.getMessage(), ex);
-        }
-        System.out.println("Target=============================================== Print object");
-        writer.write(System.out, definition.getTestBox(), root);
-
-        System.out.println("Target=============================================== Comment");
-        assertNull(root.getOne());
-        assertNotNull(root.getList());
-        assertNotNull(root.getArr());
-        System.out.println("subsub  = " + root.getSubsub().getText());
-        for (ValueInterface elem : root.getArr()) {
-            System.out.println("elem  > " + (elem == null ? "null" : elem.getText()));
-        }
-        System.out.println("Target===============================================");
-        System.out.println(root);
-        return root;
-    }
-
-    /**
-     * Test of getModel method with testbox configuration containing errors.
-     */
-    @Test
-    public void testModel3_error() {
-        System.out.println("===============================================");
-        System.out.println("testModel3");
-        System.out.println("===============================================");
-
-        File configFile = new File("./assets/config/testbox_error.json");
-        testModel_error(configFile, ImplTestDefinition.getInstance());
     }
 
 }
