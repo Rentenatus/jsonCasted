47fa1e19430599172d987b3f82d365fad9fe8fff
Janusch Rentenatus <jrentenatus@outlook.de>
2026-08-11 20:13:06 +0200

    ... in progress...

--- src/de/jare/jsoncasted/item/builder/BuilderService.java
+++ src/de/jare/jsoncasted/item/builder/BuilderService.java
@@ -14,6 +14,7 @@ import de.jare.jsoncasted.model.descriptor.JsonTypeDescriptor;
 import de.jare.jsoncasted.model.item.JsonClass;
 import de.jare.jsoncasted.model.item.JsonInter;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -163,7 +164,8 @@ public class BuilderService {
         }
         JsonInter jInter = model.getJsonInter(typeName);
         if (jInter != null) {
-            return jInter.build(this, jsonValue.listIterator(), asList, jsonValue.listSize());
+            final Iterator<JsonItem> listIterator = jsonValue.listIterator();
+            return jInter.build(this, listIterator, asList, jsonValue.listSize());
         }
         buildException("JsonClass " + typeName + " is unknown.");
         return null;
